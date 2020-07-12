67ef5499c888d7eea9a9266e0fe09b972f56d0bf
JustinPot1213 <669308066@qq.com>
2020-07-12 12:09:30 +0800

    fix conflict
    Signed-off-by: JustinPot1213 <669308066@qq.com>

--- UPCSPlatForm/CoursePart/admin.py
+++ UPCSPlatForm/CoursePart/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 
+<<<<<<< Updated upstream
 # Begin
 # auth:gz
 # create date:7.11
 # description:管理课程
 
+=======
+>>>>>>> Stashed changes
 
 from . import models
 
 admin.site.register(models.Course)
+<<<<<<< Updated upstream
 
 # End
+=======
+>>>>>>> Stashed changes
