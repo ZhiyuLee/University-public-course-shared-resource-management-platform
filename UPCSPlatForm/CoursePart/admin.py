b7d898f5916fd6638b21d44d05f60d4e96558faa
JustinPot1213 <669308066@qq.com>
2020-07-12 11:57:59 +0800

    Update Course Administration and a new courserListWeb
    Signed-off-by: JustinPot1213 <669308066@qq.com>

--- UPCSPlatForm/CoursePart/admin.py
+++ UPCSPlatForm/CoursePart/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Begin
+# auth:gz
+# create date:7.11
+# description:管理课程
+
+
+from . import models
+
+admin.site.register(models.Course)
+
+# End
